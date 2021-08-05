d25330aafc34059ffeb9285c7ed8cc514c644a6a
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-08-05 13:04:25 +0530

    Armstrong_number

---
+++ Armstrong.java
@@ -0,0 +1,15 @@
+// this program print the number of Armstrong or not
+package com.company;
+import java.util.Scanner;
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a, b, sum=0;
+        System.out.println("Enter the number");
+        a= sc.nextInt();
+        b=a;
+        while (a>0){
+
+        }
+    }
+}
