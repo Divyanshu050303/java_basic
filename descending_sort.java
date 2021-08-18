76d9e37b3b726a22cb42c8c362cbaf6346054f35
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-08-18 21:03:26 +0530

    descending_sort

---
+++ descending_sort.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.Scanner;
+public class descending_sort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int i , j, n;
+        System.out.println("Enter the number of element in the array...");
+        n= sc.nextInt();
+        int [] array=new int[n];
+        for(i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+        for(i=0; i<n;i++){
+
+            for(j=i+1;j< array.length;j++){
+                int temp=0;
+                if (array[i]>array[j]){
+                    temp=array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+
+            }
+            System.out.print(array[i]+" ");
+        }
+
+
+    }
+
+}
