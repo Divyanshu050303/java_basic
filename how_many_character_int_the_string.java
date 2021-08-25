d34537dee0e4ca17226cd440f4f2929c87a6c800
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-08-25 20:31:26 +0530

    how_many_character_int_the_string

---
+++ how_many_character_int_the_string.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.Scanner;
+public class how_many_character_int_the_string {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s= sc.nextLine();
+        System.out.println(s.length());
+    }
+}
